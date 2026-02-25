6918f9866553557ee8551987f1b503e38aaa6832
b1itz7 <b1itz@vip.qq.com>
2026-02-25 09:00:29 +0800

    fix: fix bug

--- fastCloud-visual/fastCloud-monitor/src/main/java/cc/ashclaw/fastCloud/monitor/MonitorApplication.java
+++ fastCloud-visual/fastCloud-monitor/src/main/java/cc/ashclaw/fastCloud/monitor/MonitorApplication.java
@@ -1,8 +1,8 @@
-package cc.ashclaw;
+package cc.ashclaw.fastCloud.monitor;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class MonitorApplication {
     static void main() {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
--- fastCloud-visual/fastCloud-seata/src/main/java/cc/ashclaw/fastCloud/seata/SeataApplication.java
+++ fastCloud-visual/fastCloud-seata/src/main/java/cc/ashclaw/fastCloud/seata/SeataApplication.java
@@ -1,8 +1,8 @@
-package cc.ashclaw;
+package cc.ashclaw.fastCloud.seata;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class SeataApplication {
     static void main() {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
